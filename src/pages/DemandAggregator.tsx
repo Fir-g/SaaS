ed3050b32a50f6f743ddd13c9178b74957684138
Firoj Ahamad <firojahamad0015@gmail.com>
2025-09-04 05:19:40 +0530

    ef

--- src/pages/DemandAggregator.tsx
+++
@@ -1,9 +0,0 @@
-import DemandAggregator from "@/components/DemandAggregator/demandaggregator";
-
-export default function DemandAggregatorPage() {
-  return (
-    <div>
-      <DemandAggregator />
-    </div>
-  );
-}
--- src/validations/loginSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  username: z.string().min(1, "Username is required"),
-  password: z.string().min(1, "Password must be at least 6 characters"),
-  crmLink: z.string().url("Enter a valid CRM URL"),
-});
-
-export type LoginFormValues = z.infer<typeof loginSchema>;
